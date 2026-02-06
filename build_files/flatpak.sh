7321c33257729bb5411f51cc265d85ac4235e65d
Mateo Orlaineta <mateowoetam@proton.me>
2026-02-06 10:14:08 -0600

    edit flatpak script

--- build_files/flatpak.sh
+++ build_files/flatpak.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-set -eu
-flatpak remote-add --if-not-exists \
-flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+set -euxo pipefail
+flatpak --system remote-add --if-not-exists \
+flathub https://dl.flathub.org/repo/flathub.flatpakrepo||true
 for app in \
 org.fkoehler.KTailctl \
 org.deskflow.deskflow \
@@ -9,18 +9,15 @@ org.gnome.DejaDup \
 org.kde.haruna \
 org.mozilla.Thunderbird \
 org.mozilla.firefox;do
-if flatpak info "$app" >/dev/null 2>&1;then
-flatpak remove -y "$app"
-fi
+flatpak --system uninstall -y "$app"||true
 done
-flatpak install -y --noninteractive flathub \
+flatpak --system install -y --noninteractive flathub \
 io.github.kolunmi.Bazaar \
 io.github.flattool.Warehouse \
 com.belmoussaoui.Authenticator \
 org.libreoffice.LibreOffice \
 com.nextcloud.desktopclient.nextcloud \
 org.localsend.localsend_app \
-app.comaps.comaps \
 org.kde.elisa \
-org.videolan.VLC
-flatpak uninstall --unused -y||true
+org.videolan.VLC||true
+flatpak --system uninstall --unused -y||true
