d138a8de21083c1c0421470c5b00db8c37337d22
Mateo Orlaineta <mateowoetam@proton.me>
2026-02-06 11:03:17 -0600

    flatpak changes

--- build_files/flatpak.sh
+++ build_files/flatpak.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 export FLATPAK_DISABLE_SANDBOX=1
-mkdir -p /root/.cache/dconf
 flatpak --system remote-add --if-not-exists \
 flathub https://dl.flathub.org/repo/flathub.flatpakrepo||true
 for app in \
@@ -14,5 +13,7 @@ org.mozilla.firefox;do
 flatpak --system uninstall -y "$app"||true
 done
 mkdir -p /etc/flatpak/preinstall.d
-cp -r /ctx/custom/flatpaks/*.preinstall /etc/flatpak/preinstall.d/
+if compgen -G "/ctx/custom/flatpaks/*.preinstall" >/dev/null;then
+cp /ctx/custom/flatpaks/*.preinstall /etc/flatpak/preinstall.d/
+fi
 flatpak --system uninstall --unused -y||true
