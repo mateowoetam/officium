e78773c36fd76d32d50ddf930a462521d797f84d
Mateo Orlaineta <mateowoetam@proton.me>
2026-02-07 20:19:23 -0600

    edited nvidia.sh

--- build_files/nvidia.sh
+++ build_files/nvidia.sh
@@ -6,12 +6,10 @@ DNF_OPTS="--setopt=tsflags=nodocs --setopt=install_weak_deps=False"
 dnf5 -y install --refresh $DNF_OPTS --allowerasing \
 "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$FEDORA_VERSION.noarch.rpm" \
 "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$FEDORA_VERSION.noarch.rpm"
+dnf5 remove -y '*nvidia*'||true
 dnf5 -y install --refresh $DNF_OPTS --allowerasing --best \
-kernel-core \
 kernel-devel-matched \
-kernel-headers \
 akmod-nvidia \
-xorg-x11-drv-nvidia \
 xorg-x11-drv-nvidia-cuda
 akmods --force --kernels "$(rpm -q kernel-core --qf '%{VERSION}-%{RELEASE}.%{ARCH}\n')"
 dnf5 remove -y rpmfusion-free-release rpmfusion-nonfree-release
