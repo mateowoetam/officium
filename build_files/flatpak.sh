c1fffaac2ffe929e8c85df9ca8ef6c166cc143b4
Mateo Orlaineta <mateowoetam@proton.me>
2026-02-11 10:47:37 -0600

    updated flatpak.sh

--- build_files/flatpak.sh
+++ build_files/flatpak.sh
@@ -12,7 +12,7 @@ dnf5 copr enable -y ublue-os/flatpak-test
 dnf5 copr disable -y ublue-os/flatpak-test||true
 dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak flatpak
 dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-libs flatpak-libs
-dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test flatpak-session-helper flatpak-session-helper
+dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-session-helper flatpak-session-helper
 flatpak --system remote-add --if-not-exists \
 flathub https://dl.flathub.org/repo/flathub.flatpakrepo||true
 flatpak update --appstream
