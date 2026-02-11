05e8e6478525c927b9d3d292ce315dcd223c32ad
Mateo Orlaineta <mateowoetam@proton.me>
2026-02-10 19:44:37 -0600

    flatpak changes

--- build_files/flatpak.sh
+++ build_files/flatpak.sh
@@ -3,10 +3,6 @@ set -euxo pipefail
 export FLATPAK_DISABLE_SANDBOX=1
 flatpak --system remote-add --if-not-exists \
 flathub https://dl.flathub.org/repo/flathub.flatpakrepo||true
-dnf5 copr enable copr.fedorainfracloud.org/ublue-os/packages
-dnf5 install krunner-bazaar
-dnf5 copr disable copr.fedorainfracloud.org/ublue-os/packages
-dnf5 copr remove copr.fedorainfracloud.org/ublue-os/packages
 mkdir -p /etc/flatpak/preinstall.d
 if compgen -G "/ctx/custom/flatpaks/*.preinstall" >/dev/null;then
 cp /ctx/custom/flatpaks/*.preinstall /etc/flatpak/preinstall.d/
