dcb64d8361d90b5405eb49a74953d7917c86e270
Mateo Orlaineta <mateowoetam@proton.me>
2026-02-11 01:52:31 -0600

    updated flatpak.sh

--- build_files/flatpak.sh
+++ build_files/flatpak.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 export FLATPAK_DISABLE_SANDBOX=1
+dnf -y install 'dnf5-command(config-manager)'
 rm -rf /etc/yum.repos.d/_copr_ublue-os-akmods.repo
-dnf5 copr enable ublue-os/akmods
+dnf5 copr enable ublue-os/akmods||true
 dnf5 copr disable ublue-os/akmods||true
-dnf5 copr enable ublue-os/packages
+dnf5 copr enable ublue-os/packages||true
 dnf5 copr disable ublue-os/packages||true
 dnf5 -y --enablerepo copr:copr.fedorainfracloud.org:ublue-os:packages install uupd ublue-os-udev-rules
-dnf5 copr enable ublue-os/flatpak-test
+dnf5 copr enable ublue-os/flatpak-test||true
 dnf5 copr disable ublue-os/flatpak-test||true
-dnf5 -y --enablerepo copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak flatpak
-dnf5 -y --enablerepo copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-libs flatpak-libs
-dnf5 -y --enablerepo copr:copr.fedorainfracloud.org:ublue-os:flatpak-test flatpak-session-helper flatpak-session-helper
+dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak flatpak
+dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-libs flatpak-libs
+dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test flatpak-session-helper flatpak-session-helper
 flatpak --system remote-add --if-not-exists \
 flathub https://dl.flathub.org/repo/flathub.flatpakrepo||true
 flatpak update --appstream
