752c57a04ca797b6a6ecca16804501e8e6a9c37e
Mateo Orlaineta <mateowoetam@proton.me>
2026-02-11 22:22:35 -0600

    made scripts use /usr/bin/env bash

--- build_files/mount-nix-overlay.sh
+++ build_files/mount-nix-overlay.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 set -e
 mkdir -p /usr/share/nix-store
 mkdir -p /var/lib/nix-store
--- build_files/nix.sh
+++ build_files/nix.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 dnf install -y https://nix-community.github.io/nix-installers/nix/x86_64/nix-multi-user-2.24.10.rpm
 if [ -d /nix/store ];then
 mv /nix/* /usr/share/nix-store/
