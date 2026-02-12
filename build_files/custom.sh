053d4de22b11d1ff2dcba24da585125f16a47218
Mateo Orlaineta <mateowoetam@proton.me>
2026-02-11 19:21:20 -0600

    change pretty name os-release

--- build_files/custom.sh
+++ build_files/custom.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 sed -i 's/^NAME=.*/NAME="Fedora (Officium)"/' /etc/os-release
-sed -i 's/^PRETTY_NAME=.*/PRETTY_NAME="Fedora (Officium)"/' /etc/os-release
+sed -i 's/^PRETTY_NAME=.*/PRETTY_NAME="Fedora (Officium)"/' /usr/lib/os-release
+sed -i 's/^NAME=.*/NAME="Fedora (Officium)"/' /etc/os-release
+sed -i 's/^PRETTY_NAME=.*/PRETTY_NAME="Fedora (Officium)"/' /usr/lib/os-release
