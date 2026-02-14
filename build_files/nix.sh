f5f3ca614ab631ee040b8dca59ae7bb4bdc0e9f0
Mateo Orlaineta <mateowoetam@proton.me>
2026-02-14 17:41:24 -0600

    changed nix installer

--- build_files/nix.sh
+++ build_files/nix.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-dnf install -y https://nix-community.github.io/nix-installers/nix/x86_64/nix-multi-user-2.24.10.rpm
+dnf5 install -y https://nix-community.github.io/nix-installers/nix/x86_64/nix-multi-user-2.24.10.rpm
 if [ -d /nix/store ];then
 mv /nix/* /usr/share/nix-store/
 fi
+dnf5 install -y --allowerasing nix
